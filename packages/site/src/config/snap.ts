29ffa8a41a7e2e5f9b8bccf6d95f32a8af791047
Aldrick Bonaobra <git@aldrickb.com>
2025-04-08 17:59:22 +0800

    Add mode for debug and release

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -8,3 +8,6 @@
  */
 export const defaultSnapOrigin =
   process.env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
+
+export const mode = 
+  ['debug', 'release'].includes(process.env.MODE || "") ? process.env.MODE : 'debug';
